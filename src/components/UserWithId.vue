72a9c194475319f57d36d850adf204d4cf114ac9
roma_marshall <roma_marshall@outlook.com>
2024-11-04 17:30:59 +0100

    added link template

--- src/components/UserWithId.vue
+++ src/components/UserWithId.vue
@@ -22,6 +22,18 @@
       <span class="w-3/4">{{ description }}</span>
     </div>
 
+    <div class="flex flex-col mt-10 border rounded-full bg-gray-200">
+      <div class="flex justify-center my-4 relative">
+          <svg class="w-6 h-6 absolute left-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+          </svg>
+          <span>Website</span>
+      </div>
+    </div>
+
+
+
+
   </div>
 </template>
 
@@ -49,7 +61,6 @@ const getUserData = async () => {
     console.log("No user data was found")
     //todo: redirect to 404 page
   }
-
 }
 
 onMounted(() => {
