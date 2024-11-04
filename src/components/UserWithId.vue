93bc3b1deaf132ad5735592a0d380de2c97dd8a0
roma_marshall <roma_marshall@outlook.com>
2024-11-04 14:55:47 +0100

    added sign out method

--- src/components/UserWithId.vue
+++ src/components/UserWithId.vue
@@ -2,9 +2,24 @@
   <div>
     <h1>UserWithId</h1>
     <h1>The user with ID: {{ $route.params.id }}</h1>
+    <button @click="handleSignOut">sign out</button>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+import { getAuth, signOut } from 'firebase/auth'
 
+const router = useRouter()
+const auth = getAuth()
+
+const handleSignOut = async () => {
+  try {
+    await signOut(auth)
+    await router.push('/login')
+    console.log('Successfully log out!')
+  } catch (error) {
+    console.log(error.code)
+  }
+}
 </script>
