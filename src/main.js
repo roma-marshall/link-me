93ba137b63616a4226abd39951136be6d4fd7e62
roma_marshall <roma_marshall@outlook.com>
2024-11-04 14:14:40 +0100

    added firebase config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from './firebase/config'
 import router from './router/router'
 import App from './App.vue'
 
+initializeApp(firebaseConfig)
 createApp(App)
   .use(router)
   .mount('#app')
