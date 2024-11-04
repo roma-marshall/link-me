c2db7609404747b7efdf0a961ebd3d483dfabb20
roma_marshall <roma_marshall@outlook.com>
2024-11-04 17:55:47 +0100

    refactoring profile page: added setting

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,22 +1,105 @@
 <template>
-  <div class="flex flex-col">
-    <div class="flex justify-around my-10">
-      <a @click="router.push(`/user/${auth.currentUser.uid}`)" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go to your page</a>
-      <a @click="handleSignOut" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Out</a>
+  <div>
+    <div class="flex flex-col">
+      <div class="flex justify-around my-10">
+        <a @click="router.push(`/user/${auth.currentUser.uid}`)" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go to your page</a>
+        <a @click="handleSignOut" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Out</a>
+      </div>
+      <div class="flex flex-col mx-auto my-10">
+        <div>EmailVerified: {{ auth.currentUser.emailVerified }}</div>
+        <div>The user with ID: {{ auth.currentUser.uid }}</div>
+      </div>
     </div>
-    <div class="flex flex-col mx-auto my-10">
-      <div>EmailVerified: {{ auth.currentUser.emailVerified }}</div>
-      <div>The user with ID: {{ auth.currentUser.uid }}</div>
+
+    <hr class="mb-10" />
+
+    <div class="flex flex-col justify-center mx-5">
+
+      <div class="mx-auto">
+        <svg class="w-20 h-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+      </div>
+
+      <div class="flex flex-col items-center text-center space-y-5">
+
+        <!-- Username -->
+        <div class="max-w-sm mx-auto w-full">
+          <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+          <div class="flex">
+            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+              <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+
+            </span>
+            <input v-model="username" type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="@username">
+          </div>
+        </div>
+
+        <!-- Description -->
+        <div class="max-w-sm mx-auto w-full">
+          <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+          <div class="flex">
+            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+              <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+              </svg>
+            </span>
+            <textarea v-model="description" v-html="description" type="text" id="website-admin" class="resize-none rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="@username"></textarea>
+          </div>
+        </div>
+
+        <!-- Save data -->
+        <a @click="" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</a>
+
+      </div>
+
     </div>
+
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+
 import { useRouter } from 'vue-router'
 import { getAuth, signOut } from 'firebase/auth'
 
 const router = useRouter()
 const auth = getAuth()
+const route = useRoute()
+const db = getFirestore()
+const uid = ref()
+const username = ref()
+const description = ref()
+
+const getUserData = async () => {
+  const user = auth.currentUser
+
+  if (user) {
+    const uid = user.uid
+    const userDoc = await getDoc(doc(db, "users", uid))
+
+    if (userDoc.exists()) {
+      const userData = userDoc.data()
+      username.value = userData.username
+      description.value = userData.description
+    } else {
+      console.log("No user data was found")
+    }
+  }
+}
+
+onMounted(() => {
+  uid.value = route.params.id
+  getUserData()
+})
+
+
+
 
 const handleSignOut = async () => {
   try {
--- src/components/UserWithId.vue
+++ src/components/UserWithId.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="flex flex-col">
-    <div class="md:flex justify-around my-10">
-      <a @click="$router.push('/profile')" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Settings</a>
-      <div>Public page</div>
-      <div>The user with ID: {{ $route.params.id }}</div>
-      <div>The user with ID: {{ uid }}</div>
-    </div>
-  </div>
-  <hr class="mb-10" />
-
-  <div class="flex flex-col justify-center mx-5">
-
-    <div class="mx-auto">
-      <svg class="w-20 h-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-      </svg>
+  <div>
+    <div class="flex flex-col">
+      <div class="md:flex justify-around my-10">
+        <a @click="$router.push('/profile')" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Settings</a>
+        <div>Public page</div>
+        <div>The user with ID: {{ $route.params.id }}</div>
+        <div>The user with ID: {{ uid }}</div>
+      </div>
     </div>
 
-    <div class="flex flex-col items-center text-center mx-auto">
-      <span class="text-lg font-bold">@{{ username }}</span>
-      <span class="w-3/4">{{ description }}</span>
-    </div>
+    <hr class="mb-10" />
 
-    <div class="flex flex-col mt-10">
-      <a href="#" target="_blank" class="flex justify-center py-4 md:w-1/2 md:mx-auto relative border border-black rounded-full bg-gray-200 cursor-pointer">
-        <svg class="w-6 h-6 absolute left-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+    <div class="flex flex-col justify-center mx-5">
+
+      <div class="mx-auto">
+        <svg class="w-20 h-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         </svg>
-        <span>Website</span>
-      </a>
+      </div>
+
+      <div class="flex flex-col items-center text-center mx-auto">
+        <span class="text-lg font-bold">@{{ username }}</span>
+        <span class="w-3/4">{{ description }}</span>
+      </div>
+
+      <div class="flex flex-col mt-10">
+        <a href="#" target="_blank" class="flex justify-center py-4 md:w-1/2 md:mx-auto relative border border-black rounded-full bg-gray-200 cursor-pointer">
+          <svg class="w-6 h-6 absolute left-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+          </svg>
+          <span>Website</span>
+        </a>
+      </div>
+
     </div>
 
   </div>
@@ -52,8 +56,6 @@ const getUserData = async () => {
     const userData = userDoc.data()
     username.value = userData.username
     description.value = userData.description
-    console.log("username:", userData.username)
-    console.log("description:", userData.description)
   } else {
     console.log("No user data was found")
     //todo: redirect to 404 page
@@ -64,5 +66,4 @@ onMounted(() => {
   uid.value = route.params.id
   getUserData()
 })
-
 </script>
