fa87fc859d640176573e809ee475cadda1a1d86a
roma_marshall <roma_marshall@outlook.com>
2024-11-04 15:37:26 +0100

    refactoring profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex flex-col">
     <div class="flex justify-around my-10">
-      <h1>UserWithId</h1>
-      <h1>The user with ID: {{ $route.params.id }}</h1>
       <a @click="router.push(`/user/${auth.currentUser.uid}`)" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Go to your page</a>
       <a @click="handleSignOut" class="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Out</a>
+    </div>
+    <div class="flex flex-col mx-auto my-10">
+      <div>EmailVerified: {{ auth.currentUser.emailVerified }}</div>
+      <div>The user with ID: {{ auth.currentUser.uid }}</div>
     </div>
   </div>
 </template>
--- src/components/UserWithId.vue
+++ src/components/UserWithId.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-col">
     <div class="flex justify-around my-10">
       <div>Public page</div>
+      <a @click="$router.push('/profile')">settings</a>
       <h1>The user with ID: {{ $route.params.id }}</h1>
     </div>
   </div>
