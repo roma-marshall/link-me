b8e38e447c5cfabf44b50aad1fdd6965a81e5e89
roma_marshall <roma_marshall@outlook.com>
2024-11-09 16:11:19 +0100

    code clean up

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,6 +6,6 @@ const firebaseConfig = {
   storageBucket: "link-me-2b6f2.firebasestorage.app",
   messagingSenderId: "563678849311",
   appId: "1:563678849311:web:fad8dd60ece545974c682a"
-};
+}
 
 export default firebaseConfig
