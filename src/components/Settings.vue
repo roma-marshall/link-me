58ddbcf56abdbd5432b23c41cb68bbf6266b8051
roma_marshall <roma_marshall@outlook.com>
2024-11-13 19:04:49 +0100

    added tailwind @apply config

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -30,7 +30,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               </svg>
             </span>
-            <input v-model="username" type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="@username">
+            <input v-model="username" type="text" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="@username">
           </div>
         </div>
 
@@ -42,7 +42,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
               </svg>
             </span>
-            <textarea v-model="description" v-html="description" type="text" id="website-admin" class="resize-none rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="description"></textarea>
+            <textarea v-model="description" v-html="description" type="text" class="resize-none rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="description"></textarea>
           </div>
         </div>
 
@@ -56,8 +56,8 @@
               </svg>
             </span>
               <div class="flex flex-col w-full">
-                <input @keyup.enter="saveUserData" v-model="title[index]" type="text" id="website-admin" class="rounded-none bg-gray-50 border-b-0 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="title">
-                <input @keyup.enter="saveUserData" v-model="link[index]" type="text" id="website-admin" class="rounded-none bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="link">
+                <input @keyup.enter="saveUserData" v-model="title[index]" type="text" class="rounded-none bg-gray-50 border-b-0 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="title">
+                <input @keyup.enter="saveUserData" v-model="link[index]" type="text" class="rounded-none bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-base p-2.5" placeholder="link">
               </div>
               <span @click="removeLink(index)" class="cursor-pointer inline-flex items-center px-3 text-base text-gray-900 bg-gray-200 border border-s-0 border-gray-300 rounded-e-md">
                 <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -75,9 +75,7 @@
         </a>
 
         <!-- Save data -->
-        <a @click="saveUserData" class="btn">
-          Save
-        </a>
+        <a @click="saveUserData" class="btn">Save</a>
 
       </div>
     </div>
